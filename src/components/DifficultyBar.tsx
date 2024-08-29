8cc07fc5fcd41b324f85386b9b6c4cef0db238e1
ari <wnabwt@yahoo.com>
2024-08-29 13:21:54 +0800

    refactor: Used Tabs component.

---
+++ src/components/DifficultyBar.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Tabs, TabsList, TabsTrigger } from './ui/tabs';
+import { TDifficulty } from '@/types';
+
+type DifficultyBarProps = {
+    difficulty: TDifficulty;
+    onDifficultyChange: (t: string) => void
+}
+
+export const DifficultyBar = ({difficulty, onDifficultyChange}: DifficultyBarProps) => {
+  return (
+    <div className="pb-10">
+            <Tabs value={difficulty} onValueChange={onDifficultyChange}>
+              <TabsList className="grid w-full grid-cols-3 rounded p-2">
+                <TabsTrigger value="easy" >
+                  Easy
+                </TabsTrigger>
+                <TabsTrigger value="medium" >
+                  Medium
+                </TabsTrigger>
+                <TabsTrigger value="hard"  >
+                  Hard
+                </TabsTrigger>
+              </TabsList>
+            </Tabs>
+          </div>
+  )
+}
