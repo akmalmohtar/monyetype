5bfe02c24e8dbd3992adf6819d68a1a34b4b4277
ari <wnabwt@yahoo.com>
2024-08-31 11:44:11 +0800

    refactor: removed react hook form

--- src/components/SettingModal.tsx
+++ src/components/SettingModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import { useForm } from "react-hook-form";
 import {
   Dialog,
@@ -17,6 +17,15 @@ import { Checkbox } from "./ui/checkbox";
 import type { TRhythmSettings } from "@/types";
 import { useRhythmSettingsStore } from "@/hooks/zustand/use-rhythm-settings";
 
+const initialValues: TRhythmSettings = {
+  gameDuration: 30000,
+  letterDuration: 1500,
+  enableNumbers: false,
+  enableSpecialCharacters: false,
+  enableUppercaseLetters: false,
+  enableUppercaseSpecialCharacters: false,
+};
+
 export default function SettingModal() {
   const [open, setOpen] = useState(false);
   const { rhythmSettings, saveSettings } = useRhythmSettingsStore((state) => ({
@@ -24,17 +33,7 @@ export default function SettingModal() {
     saveSettings: state.saveSettings,
   }));
 
-  const {
-    getValues,
-    register,
-    handleSubmit,
-    setValue,
-    formState: { errors, isDirty },
-  } = useForm<TRhythmSettings>({
-    defaultValues: rhythmSettings,
-  });
-
-  const onSubmit = (values: TRhythmSettings) => {
+  const handleSubmit = (values: TRhythmSettings) => {
     saveSettings(values);
     setOpen(false);
   };
@@ -53,98 +52,58 @@ export default function SettingModal() {
             Configure your gameplay experience
           </DialogDescription>
         </DialogHeader>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col space-y-4"
-        >
-          <div className="grid grid-cols-2 space-x-4">
-            <Label htmlFor="game-duration">Game duration in milliseconds</Label>
-            <Input
-              id="game-duration"
-              type="number"
-              min={0}
-              {...register("gameDuration")}
-            />
-            {errors.gameDuration && (
-              <p className="text-red-500">{errors.gameDuration.message}</p>
-            )}
-          </div>
 
-          <div className="grid grid-cols-2 space-x-4">
-            <Label htmlFor="letter-duration">
-              Duration per letter in milliseconds
-            </Label>
-            <Input
-              id="letter-duration"
-              type="number"
-              min={0}
-              {...register("letterDuration")}
-            />
-            {errors.letterDuration && (
-              <p className="text-red-500">{errors.letterDuration.message}</p>
-            )}
-          </div>
+        <div className="grid grid-cols-2 space-x-4">
+          <Label htmlFor="game-duration">Game duration in milliseconds</Label>
+          <Input id="game-duration" type="number" min={0} />
+        </div>
 
-          <div className="flex space-x-2">
-            <Checkbox
-              id="numbers-enable"
-              onCheckedChange={(value) => setValue("enableNumbers", !!value)}
-              defaultChecked={getValues("enableNumbers")}
-              {...register("enableNumbers")}
-            />
-            <Label htmlFor="numbers-enable">Enable numbers</Label>
-          </div>
+        <div className="grid grid-cols-2 space-x-4">
+          <Label htmlFor="letter-duration">
+            Duration per letter in milliseconds
+          </Label>
+          <Input id="letter-duration" type="number" min={0} />
+        </div>
 
-          <div className="flex space-x-2">
-            <Checkbox
-              id="special-chars-enable"
-              onCheckedChange={(value) =>
-                setValue("enableSpecialCharacters", !!value)
-              }
-              defaultChecked={getValues("enableSpecialCharacters")}
-              {...register("enableSpecialCharacters")}
-            />
-            <Label htmlFor="special-chars-enable">
-              Enable special characters
-            </Label>
-          </div>
+        <div className="flex space-x-2">
+          <Checkbox id="numbers-enable" onCheckedChange={(value) => {}} />
+          <Label htmlFor="numbers-enable">Enable numbers</Label>
+        </div>
 
-          <div className="flex space-x-2">
-            <Checkbox
-              id="uppercase-chars-enable"
-              onCheckedChange={(value) =>
-                setValue("enableUppercaseLetters", !!value)
-              }
-              defaultChecked={getValues("enableUppercaseLetters")}
-              {...register("enableUppercaseLetters")}
-            />
-            <Label htmlFor="uppercase-chars-enable">
-              Enable uppercase letters
-            </Label>
-          </div>
+        <div className="flex space-x-2">
+          <Checkbox id="special-chars-enable" onCheckedChange={(value) => {}} />
+          <Label htmlFor="special-chars-enable">
+            Enable special characters
+          </Label>
+        </div>
 
-          <div className="flex space-x-2">
-            <Checkbox
-              id="uppercase-special-chars-enable"
-              defaultChecked={getValues("enableUppercaseSpecialCharacters")}
-              {...register("enableUppercaseSpecialCharacters")}
-            />
-            <Label htmlFor="uppercase-special-chars-enable">
-              Enable uppercase special characters (special characters that
-              requires you to hold shift)
-            </Label>
-          </div>
-          <DialogFooter className="sm:justify-start">
-            <DialogClose asChild>
-              <Button type="button" variant="secondary">
-                Close
-              </Button>
-            </DialogClose>
-            <Button variant={"akmalmohtar"} type="submit" disabled={!isDirty}>
-              Save
+        <div className="flex space-x-2">
+          <Checkbox
+            id="uppercase-chars-enable"
+            onCheckedChange={(value) => {}}
+          />
+          <Label htmlFor="uppercase-chars-enable">
+            Enable uppercase letters
+          </Label>
+        </div>
+
+        <div className="flex space-x-2">
+          <Checkbox id="uppercase-special-chars-enable" />
+          <Label htmlFor="uppercase-special-chars-enable">
+            Enable uppercase special characters (special characters that
+            requires you to hold shift)
+          </Label>
+        </div>
+        <DialogFooter className="sm:justify-start">
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Close
             </Button>
-          </DialogFooter>
-        </form>
+          </DialogClose>
+          <Button variant={"akmalmohtar"} type="submit">
+            Save
+          </Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
